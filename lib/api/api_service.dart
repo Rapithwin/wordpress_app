367ff833ed34122ae6ffa07eac815b07ecc419de
unknown <khandanamir7@gmail.com>
2024-09-11 21:35:33 +0330

    Login future created

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wordpress_app/constants/constants.dart';
 import 'package:wordpress_app/models/woocommerce/costumer_model.dart';
+import 'package:wordpress_app/models/woocommerce/login_model.dart';
 
 class APIService {
   final String consumerKey = dotenv.env["CONSUMER_KEY"]!;
@@ -21,7 +22,10 @@ class APIService {
         data: model.toJson(),
         options: Options(
           method: "POST",
-          headers: {HttpHeaders.authorizationHeader: "Basic $authToken"},
+          headers: {
+            HttpHeaders.authorizationHeader: "Basic $authToken",
+            HttpHeaders.contentTypeHeader: "application/json",
+          },
         ),
       );
       if (response.statusCode == 201) {
@@ -32,5 +36,34 @@ class APIService {
       debugPrint(e.toString());
     }
     return isCreated;
+  }
+
+  Future<LoginModel> loginCustomer(
+    String username,
+    String password,
+  ) async {
+    late LoginModel loginModel;
+
+    try {
+      var response = await Dio().request(
+        WoocommerceInfo.jwtUrl,
+        data: {
+          "username": username,
+          "password": password,
+        },
+        options: Options(
+          method: "POST",
+          headers: {
+            HttpHeaders.contentTypeHeader: "application/json",
+          },
+        ),
+      );
+      if (response.statusCode == 201) {
+        loginModel = LoginModel.fromJson(response.data);
+      }
+    } on DioException catch (e) {
+      debugPrint(e.toString());
+    }
+    return loginModel;
   }
 }
