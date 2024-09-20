16fb10ac83685c82aac48af88a277d63f8fd060a
unknown <khandanamir7@gmail.com>
2024-09-20 21:59:13 +0330

    Farsi number extention

--- lib/utils/extention.dart
+++ lib/utils/extention.dart
@@ -5,3 +5,16 @@ extension ExtString on String {
     return emailRegEx.hasMatch(this);
   }
 }
+
+extension FarsiNumber on String {
+  String get farsiNumber {
+    const eng = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const farsi = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    String text = this;
+
+    for (int i = 0; i < eng.length; i++) {
+      text = text.replaceAll(eng[i], farsi[i]);
+    }
+    return text;
+  }
+}
