abcce41570df0bce4da1d037144a76668fa3f014
unknown <khandanamir7@gmail.com>
2024-10-17 00:42:36 +0330

    Added the loader provider to product detail page

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart' show NumberFormat;
+import 'package:provider/provider.dart';
 import 'package:readmore/readmore.dart';
 import 'package:wordpress_app/models/woocommerce/products_model.dart';
+import 'package:wordpress_app/provider/loader_provider.dart';
 import 'package:wordpress_app/utils/custom_appbar.dart';
 import 'package:wordpress_app/utils/extention.dart';
 import 'package:wordpress_app/widgets/custom_bottom_appbar.dart';
@@ -20,83 +22,86 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
     Size size = MediaQuery.of(context).size;
     TextTheme textTheme = Theme.of(context).textTheme;
     final NumberFormat numberFormat = NumberFormat.decimalPattern("fa");
-    return Scaffold(
-      appBar: CustomAppBar.customAppBarDetail(context),
-      bottomNavigationBar: const CustomBottomAppbar(),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: size.width,
-          child: Column(
-            children: <Widget>[
-              // Image
-              SizedBox(
-                width: size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Image.network(
-                      widget.product!.images![0].src!.replaceAll(
-                        "localhost",
-                        "10.0.2.2",
+
+    return Consumer<LoaderProvider>(builder: (context, loader, child) {
+      return Scaffold(
+        appBar: CustomAppBar.customAppBarDetail(context),
+        bottomNavigationBar: const CustomBottomAppbar(),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            width: size.width,
+            child: Column(
+              children: <Widget>[
+                // Image
+                SizedBox(
+                  width: size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Image.network(
+                        widget.product!.images![0].src!.replaceAll(
+                          "localhost",
+                          "10.0.2.2",
+                        ),
+                        height: 240,
+                        width: size.width,
+                        scale: 0.5,
+                        fit: BoxFit.contain,
                       ),
-                      height: 240,
-                      width: size.width,
-                      scale: 0.5,
-                      fit: BoxFit.contain,
-                    ),
-                    // title
-                    Text(
-                      widget.product!.name ?? "null",
-                      textDirection: TextDirection.rtl,
-                      style: textTheme.titleMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
-              ),
-              //description
-              SizedBox(
-                width: size.width,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: ReadMoreText(
-                        widget.product!.description!.removeHtml,
-                        trimLines: 2,
-                        trimCollapsedText: "بیشتر",
-                        trimExpandedText: "بستن",
+                      // title
+                      Text(
+                        widget.product!.name ?? "null",
                         textDirection: TextDirection.rtl,
-                        textAlign: TextAlign.justify,
-                        moreStyle: const TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                        lessStyle: const TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 15.0),
-                      child: Text(
-                        "${numberFormat.format(
-                          int.parse(
-                            widget.product!.price!,
-                          ),
-                        )}  تومان",
                         style: textTheme.titleMedium,
+                        textAlign: TextAlign.center,
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              )
-            ],
+                //description
+                SizedBox(
+                  width: size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: ReadMoreText(
+                          widget.product!.description!.removeHtml,
+                          trimLines: 2,
+                          trimCollapsedText: "بیشتر",
+                          trimExpandedText: "بستن",
+                          textDirection: TextDirection.rtl,
+                          textAlign: TextAlign.justify,
+                          moreStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                          lessStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0),
+                        child: Text(
+                          "${numberFormat.format(
+                            int.parse(
+                              widget.product!.price!,
+                            ),
+                          )}  تومان",
+                          style: textTheme.titleMedium,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
