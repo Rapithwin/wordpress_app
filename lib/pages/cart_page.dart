a43c5e2ab0e637ef16d252c0e685e5542f4ec11b
unknown <khandanamir7@gmail.com>
2024-10-28 00:57:16 +0330

    Bottom appbar showing total price added

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' show NumberFormat;
 import 'package:provider/provider.dart';
 import 'package:wordpress_app/constants/constants.dart';
 import 'package:wordpress_app/provider/shop_provider.dart';
@@ -28,37 +29,9 @@ class _CartPageState extends State<CartPage> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     final textTheme = Theme.of(context).textTheme;
+    final NumberFormat numberFormat = NumberFormat.decimalPattern("fa");
 
     return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: Constants.primaryColor.withOpacity(0.25),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: SizedBox(
-                width: size.width * 0.75,
-                height: 52,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Constants.primaryColor,
-                    elevation: 3,
-                    side: BorderSide.none,
-                    shape: ContinuousRectangleBorder(
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                  ),
-                  child: Text(
-                    "ادامه خرید",
-                    style: textTheme.titleMedium!
-                        .copyWith(color: Colors.white, fontSize: 23),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
       body: Consumer<ShopProvider>(
         builder: (context, value, child) {
           if (value.isLoading) {
@@ -75,80 +48,147 @@ class _CartPageState extends State<CartPage> {
                     style: textTheme.titleLarge,
                   ),
                 )
-              : ListView.builder(
-                  itemCount: value.cartItems!.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
+              : Stack(
+                  children: <Widget>[
+                    ListView.builder(
+                      itemCount: value.cartItems!.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: size.height / 6,
+                            width: size.width,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(10.0),
+                              ),
+                              border: Border.all(
+                                color: Constants.primaryColor,
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                // TODO: Show quantity
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    AddQuantity(
+                                      minNumber: 0,
+                                      maxNumber: 20,
+                                      iconSize: 17,
+                                      value: value
+                                          .cartItems![index].quantity!.value!,
+                                      valueChanged: (d) {
+                                        // TODO: if value < quantity remove item. else add item.
+                                      },
+                                    ),
+                                    TextButton(
+                                      onPressed: () {},
+                                      style: ButtonStyle(
+                                        overlayColor: WidgetStateProperty.all(
+                                          Constants.primaryColor
+                                              .withOpacity(0.1),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        "حذف",
+                                        style: textTheme.labelLarge?.copyWith(
+                                          color: Colors.red,
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                SizedBox(
+                                  width: size.width / 2.3,
+                                  child: Text(
+                                    value.cartItems![index].title ?? "",
+                                    textDirection: TextDirection.rtl,
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0),
+                                  child: Image.network(
+                                    value.cartItems![index].featuredImage!
+                                        .replaceAll("localhost", "10.0.2.2"),
+                                    height: 80,
+                                    width: 80,
+                                    scale: 0.5,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    Positioned(
+                      bottom: 0,
                       child: Container(
-                        height: size.height / 6,
                         width: size.width,
+                        height: 100.0,
                         decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(10.0),
-                          ),
-                          border: Border.all(
-                            color: Constants.primaryColor,
-                          ),
+                          color: Constants.primaryColor.withOpacity(0.2),
                         ),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.center,
                           children: <Widget>[
-                            // TODO: Show quantity
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                AddQuantity(
-                                  minNumber: 0,
-                                  maxNumber: 20,
-                                  iconSize: 5,
-                                  value: 0,
-                                  valueChanged: (d) {},
-                                ),
-                                TextButton(
+                            Builder(
+                              builder: (context) {
+                                int totalPrice = 0;
+                                for (var item in value.cartItems!) {
+                                  totalPrice += item.totals!.total!;
+                                }
+                                return Text(
+                                  "${numberFormat.format(
+                                    int.parse(
+                                      totalPrice.toString(),
+                                    ),
+                                  )}تومان",
+                                  style: textTheme.labelLarge?.copyWith(
+                                    fontSize: 25,
+                                    color: Constants.blackColor,
+                                  ),
+                                  textDirection: TextDirection.rtl,
+                                );
+                              },
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Expanded(
+                              child: SizedBox(
+                                width: size.width * 0.75,
+                                height: 52,
+                                child: ElevatedButton(
                                   onPressed: () {},
-                                  style: ButtonStyle(
-                                    overlayColor: WidgetStateProperty.all(
-                                      Constants.primaryColor.withOpacity(0.1),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Constants.primaryColor,
+                                    elevation: 3,
+                                    side: BorderSide.none,
+                                    shape: ContinuousRectangleBorder(
+                                      borderRadius: BorderRadius.circular(25),
                                     ),
                                   ),
                                   child: Text(
-                                    "حذف",
-                                    style: textTheme.labelLarge?.copyWith(
-                                      color: Colors.red,
-                                    ),
+                                    "ادامه خرید",
+                                    style: textTheme.titleMedium!.copyWith(
+                                        color: Colors.white, fontSize: 23),
                                   ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              width: size.width / 2.3,
-                              child: Text(
-                                value.cartItems![index].title ?? "",
-                                textDirection: TextDirection.rtl,
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 10.0),
-                              child: Image.network(
-                                value.cartItems![index].featuredImage!
-                                    .replaceAll("localhost", "10.0.2.2"),
-                                height: 80,
-                                width: 80,
-                                scale: 0.5,
-                                fit: BoxFit.contain,
+                                ),
                               ),
                             ),
                           ],
                         ),
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 );
         },
       ),
