2a85d6551baf7e708403f3c4acd940ea6cf8c648
unknown <khandanamir7@gmail.com>
2024-10-29 22:03:12 +0330

    Added provider to catalog page

--- lib/pages/catalog_page/catalog_page.dart
+++ lib/pages/catalog_page/catalog_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart' show NumberFormat;
+import 'package:provider/provider.dart';
+import 'package:wordpress_app/provider/catalog_provider.dart';
 
 class CatalogPage extends StatefulWidget {
   const CatalogPage({super.key});
@@ -8,8 +12,40 @@ class CatalogPage extends StatefulWidget {
 }
 
 class _CatalogPageState extends State<CatalogPage> {
+  final NumberFormat numberFormat = NumberFormat.decimalPattern("fa");
+  int _page = 0;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, () async {
+      var productList = Provider.of<CatalogProvider>(context, listen: false);
+      productList.initializeData();
+      productList.setLoadingState(DataStatus.initial);
+      productList.fetchProducts(_page);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Consumer<CatalogProvider>(
+            builder: (context, productModel, child) {
+              if (productModel.allProducts.isNotEmpty &&
+                  productModel.getDataStatus() != DataStatus.initial) {
+                return Flexible(
+                  child: GridView.count(crossAxisCount: 2),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
