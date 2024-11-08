6bcb893c2d1e9dd7a8c0b151d448a223999ecda1
unknown <khandanamir7@gmail.com>
2024-11-08 20:59:10 +0330

    Create verify_address_page.dart

---
+++ lib/pages/verify_address/verify_address_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyAddressPage extends StatefulWidget {
+  const VerifyAddressPage({super.key});
+
+  @override
+  State<VerifyAddressPage> createState() => _VerifyAddressPageState();
+}
+
+class _VerifyAddressPageState extends State<VerifyAddressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
