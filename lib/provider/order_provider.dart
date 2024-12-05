fb94f3002902dbf720fce5a2bbff92f753b2f25e
unknown <khandanamir7@gmail.com>
2024-12-05 20:14:41 +0330

    Passing lineItems to CreateOrderModel in payment_options.dart

--- lib/provider/order_provider.dart
+++ lib/provider/order_provider.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:wordpress_app/api/api_service.dart';
 import 'package:wordpress_app/models/woocommerce/create_order_model.dart';
+import 'package:wordpress_app/models/woocommerce/customer_details_model.dart';
 
 class OrderProvider with ChangeNotifier {
   APIService? _apiService;
 
   bool isLoading = false;
 
-  bool? _isOrderCreated;
-  bool? get isOrderCreated => _isOrderCreated;
+  bool _isOrderCreated = false;
+  bool get isOrderCreated => _isOrderCreated;
+
+  CreateOrderModel? _orderModel;
+  CreateOrderModel? get orderModel => _orderModel;
 
   Future<void> createOrderProvider(CreateOrderModel createOrderModel) async {
-    _isOrderCreated = await _apiService?.createOrder(createOrderModel);
+    _orderModel?.shipping ??= Ing();
+    _isOrderCreated = (await _apiService?.createOrder(createOrderModel))!;
     notifyListeners();
   }
 
