7357e65d0ea290661b857f6446870bda23fa2f2f
unknown <khandanamir7@gmail.com>
2024-12-10 19:11:37 +0330

    removed useless stuff

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -31,21 +31,4 @@ class WoocommerceInfo {
 class Constants {
   static const primaryColor = Color(0xFF296e48);
   static const blackColor = Colors.black;
-
-  static String titleOne = "گیاهان را بهتر از قبل درک کن";
-  static String descriptionOne =
-      "درمورد نگهداری گل و گیاهان می‌تونی اطلاعات کسب کنی";
-  static String titleTwo = "با گیاهان جدید آشنا شو";
-  static String descriptionTwo =
-      "رز مشکی یا گل رز دوست داری؟ اینجا می‌تونی پیداش کنی";
-  static String titleThree = "با یک گل بهار نمی‌شه، گل بکار";
-  static String descriptionThree =
-      "هر گلی که نیاز داشته باشید در این اپلیکیشن پیدا می‌کنید";
-
-  static List<String> plantTypes = [
-    "|پیشنهادی|",
-    "|آپارتمانی|",
-    "|محل کار|",
-    "|باغچه‌ای|",
-  ];
 }
